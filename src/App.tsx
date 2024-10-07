ded8c006e8b2b5bb2c2663a80cc2adea9295182c
podestal <luisrodriguezpodesta@gmail.com>
2024-10-07 13:40:18 -0400

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+const App = () => {
+
+  return (
+    <div className="h-screen w-full bg-neutral-900 text-neutral-50">
+
+    </div>
+  )
+}
+
+const Board = () => {
+  return (
+    <div className="flex h-full w-full gap-3 overflow-scroll p-12">
+
+    </div>
+  )
+}
+
+export default App
