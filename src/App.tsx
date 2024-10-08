d5f4fde5c831e6d891b6ef3ea2ecb3f08a44a2e5
podestal <luisrodriguezpodesta@gmail.com>
2024-10-08 11:02:28 -0400

    basic board

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,151 @@
+import { useState } from "react"
+import { motion } from "framer-motion"
+
+const DEFAULT_CARDS = [
+  // BACKLOG
+  { title: "Look into render bug in dashboard", id: "1", column: "backlog" },
+  { title: "SOX compliance checklist", id: "2", column: "backlog" },
+  { title: "[SPIKE] Migrate to Azure", id: "3", column: "backlog" },
+  { title: "Document Notifications service", id: "4", column: "backlog" },
+  // TODO
+  {
+    title: "Research DB options for new microservice",
+    id: "5",
+    column: "todo",
+  },
+  { title: "Postmortem for outage", id: "6", column: "todo" },
+  { title: "Sync with product on Q3 roadmap", id: "7", column: "todo" },
+
+  // DOING
+  {
+    title: "Refactor context providers to use Zustand",
+    id: "8",
+    column: "doing",
+  },
+  { title: "Add logging to daily CRON", id: "9", column: "doing" },
+  // DONE
+  {
+    title: "Set up DD dashboards for Lambda listener",
+    id: "10",
+    column: "done",
+  },
+]
+
+interface Card {
+  id: string
+  title: string
+  column: string
+}
+
 const App = () => {
 
   return (
     <div className="h-screen w-full bg-neutral-900 text-neutral-50">
-
+      <Board />
     </div>
   )
 }
 
 const Board = () => {
+
+  const [cards, setCards] = useState(DEFAULT_CARDS)
+
   return (
     <div className="flex h-full w-full gap-3 overflow-scroll p-12">
-
+      <Column
+        title="Backlog"
+        column="backlog"
+        headingColor="text-neutral-500"
+        cards={cards}
+        setCards={setCards}
+      />
+      <Column
+        title="TODO"
+        column="todo"
+        headingColor="text-yellow-200"
+        cards={cards}
+        setCards={setCards}
+      />
+      <Column
+        title="In progress"
+        column="doing"
+        headingColor="text-blue-200"
+        cards={cards}
+        setCards={setCards}
+      />
+      <Column
+        title="Complete"
+        column="done"
+        headingColor="text-emerald-200"
+        cards={cards}
+        setCards={setCards}
+      />
     </div>
   )
 }
+
+interface ColumnPros {
+  title: string
+  headingColor: string
+  column: string
+  setCards: (setter: []) => void
+  cards: Card[]
+}
+
+const Column = ({ title, headingColor, column, cards, setCards }: ColumnPros) => {
+
+  const [active, setActive] = useState(false)
+  const filteredCards = cards.filter( card => card.column === column)
+
+  return (
+    <div className="w-56 shrink-0">
+      <div className="mb-3 flex items-center justify-between">
+        <h3 className={`font-medium ${headingColor}`}>{title}</h3>
+        <span className="rounded text-sm text-neutral-400">
+          {filteredCards.length}
+        </span>
+      </div>
+      <div
+        className={`h-full w-full transition-colors ${
+          active ? "bg-neutral-800/50" : "bg-neutral-800/0"
+        }`}
+      >
+        {filteredCards.map( fileteredCard => (
+          <Card 
+            key={fileteredCard.id}
+            id={fileteredCard.id}
+            title={fileteredCard.title}
+            column={fileteredCard.column}
+            handleDragStart=''
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+interface CardProps {
+  title: string
+  id: string
+  column: string
+  handleDragStart: any
+}
+
+const Card = ({ title, id, column, handleDragStart }: CardProps) => {
+  return (
+    <>
+      {/* <DropIndicator beforeId={id} column={column} /> */}
+      <motion.div
+        layout
+        layoutId={id}
+        draggable="true"
+        onDragStart={(e) => handleDragStart(e, { title, id, column })}
+        className="cursor-grab rounded border border-neutral-700 bg-neutral-800 p-3 active:cursor-grabbing"
+      >
+        <p className="text-sm text-neutral-100">{title}</p>
+      </motion.div>
+    </>
+  );
+};
 
 export default App
